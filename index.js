8639fac99bfad35a263ee61b078495bbdfe551b1
Pratap-Karmakar <pratap2501.pk@gmail.com>
2022-11-03 00:29:12 +0530

    installed passport

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-const port = 8000;
 const express = require('express');
 const app = express();
+const port = 8000;
+// we have to import database mongoose
+const db = require('./config/mongoose');
+
+
+
 
 // setting up the middleware
 app.use(express.urlencoded());
 
 
 
-app.use(express.static('./assets'));
-
+// use cookie-parser
+const cookieParser = require('cookie-parser');
+// now tell the app to use cookie-parser
+app.use(cookieParser());
 
 
 
 // as we have exported the index.js of the routes folder so now we have to access that in this index.js folder
 app.use('/', require('./routes/index'));
+
+
+// app.use(express.static('./assets'));
+
+
+
+
 // so as we've exported the express router in the index.js which is in the router folder, now we have to tell app to use it.
-const routes=require('./routes');
-app.use('/',routes);
+// const routes = require('./routes');
+// app.use('/', routes);
 
 
 
-// we have to import database mongoose
-const db=require('./config/mongoose');
 
-// use cookie-parser
-const cookieParser=require('cookie-parser');
-// now tell the app to use cookie-parser
-app.use(cookieParser());
 
 
 
