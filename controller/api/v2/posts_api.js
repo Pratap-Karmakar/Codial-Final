8a7c7ca2582a90a0b0a2abc7ec403f43be0f5160
Pratap-Karmakar <pratap2501.pk@gmail.com>
2022-11-23 19:39:40 +0530

    Playing with APIs

--- controller/api/v2/posts_api.js
+++ controller/api/v2/posts_api.js
@@ -1,4 +1,5 @@
 module.exports.index = function (req, res) {
+
     return res.json(200, {
         message: "Lists of posts from v2",
         posts: []
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -5,4 +5,7 @@ const postApi = require('../../../controller/api/v1/posts_api');
 
 router.get('/', postApi.index);
 
+// to delete a post
+router.delete('/:id',postApi.destroy);
+
 module.exports = router;
